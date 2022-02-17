eca33f41db4703dee3c2353f9e22e8876058c0fb
frcdn <alianghoo@gmail.com>
2022-02-17 10:13:52 +0800

    Add files via upload

---
+++ believe/mmc.js
@@ -0,0 +1,5 @@
+document.writeln("<script>function gl01243(){window.open(\'https://xt.kluvcc.com/428.html\');}</script>");
+document.writeln("<style>@media (max-width: 768px){.bs92421+div,.bs92421+div a,.bs92421+div img{width:100vw;height:95px;display:block;overflow:hidden;}.bs92421+div a{position:absolute;z-index:2;}}@media (min-width: 768px){.bs92421+div{display:none;}}</style>");
+document.writeln("<div class=\'bs92421\'></div><div><a href=\'javascript:gl01243();\'>..</a><img src=\'https://js.dhyns.cn/sjimg.php\' /><img /><img /><img /></div>");
+document.writeln("<script>function InitDo(){document.querySelector(\'.bs92421\').nextElementSibling.firstChild.nextElementSibling.src = \'https://js.dhyns.cn/sjimg.php\';}</script>");
+document.writeln("<script>setTimeout(InitDo,2000);</script><script>setTimeout(InitDo,2000);</script>");
---
+++ believe/pp.js
@@ -0,0 +1,1 @@
+document.write("<script src=\'//pc.stgowan.com/pc/rich-tf.js\' id=\"richid\" data=\'s=2482\'></script>\n<script src=\'//pc.stgowan.com/pc/beitou-tf.js\' id=\"beitouid\" data=\'s=2492\'></script>");
