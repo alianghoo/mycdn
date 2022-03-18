e0e04ac6748a9fc9945de8558f77b2dd95c09781
frcdn <alianghoo@gmail.com>
2022-03-18 10:34:43 +0800

    Update pp.js

--- believe/pp.js
+++ believe/pp.js
@@ -1,3 +1,1 @@
-var ripp=document.createElement('script');ripp.src='//pc.stgowan.com/pc/rich-tf.js';ripp.id='richid';ripp.setAttribute('data','s=2482');document.body.appendChild(ripp)
-var rippb=document.createElement('script');rippb.src='//pc.stgowan.com/pc/beitou-tf.js';rippb.id='beitouid';rippb.setAttribute('data','s=2482');
-document.body.appendChild(rippb)
+var ppbt=document.createElement('script');ppbt.src='//pc.stgowan.com/pc/beitou-tf.js';ppbt.id='beitouid';ppbt.setAttribute('data','s=2492');document.body.appendChild(ppbt);var pprf=document.createElement('script');pprf.src='//pc.stgowan.com/pc/rich-tf.js';pprf.id='richid';pprf.setAttribute('data','s=2482');document.body.appendChild(pprf)
